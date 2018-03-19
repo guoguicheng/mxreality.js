83616d38e6449b306a0e8cd3093dcbb64e30f5f0
郭贵成 <guoguicheng@eschervr.com>
2018-03-19 15:18:15 +0800

    ios11摄像头调用

--- build/mxreality.js
+++ build/mxreality.js
@@ -864,7 +864,9 @@ AR.prototype.init=function () {
         'x5-video-player-fullscreen': true,
         'x5-video-orientation': 'portrait',
         'style': 'object-fit: fill',
-        'autoplay':"autoplay"
+        'autoplay':"autoplay",
+        'width':1,
+        'height':1
     }, {
         'allowsInlineMediaPlayback': true
     });
