9794d8cc6943bec5c1801211cc4c5bdbf35cffd2
郭贵成 <guoguicheng@eschervr.com>
2018-03-19 16:26:49 +0800

    ios11摄像头调用

--- build/mxreality.js
+++ build/mxreality.js
@@ -870,7 +870,7 @@ AR.prototype.init=function () {
         'allowsInlineMediaPlayback': true
     });
 
-    this.video.style.display = "block";
+    this.video.style.display = "none";
     document.body.appendChild(this.video);
     this.video.oncanplaythrough = function () {
         self.cameraReady = true;
@@ -893,7 +893,7 @@ AR.prototype.init=function () {
             audio: self.openAudio,
             video: {
                 facingMode: {
-                    exact: "user"
+                    exact: self.cameraIndex?"environment":"user"
                 }
             }
         }
